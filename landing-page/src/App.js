c0c9d2cdb76ac8fcaf79ee16c7789a32e891002a
Kinjal Pipaliya <kinjalpipaliya88@gmail.com>
2024-02-05 12:09:44 +0400

    Initial Commit

---
+++ landing-page/src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App() {
+  return (
+    <body class="text-center">
+      <button type = "button" class = "btn btn-primary btn-block text-white">Button CTA</button>
+    </body>
+  );
+}
+
+export default App;
