05b84116d51dca09f154b2dd7f2a20d81fcfad76
Kinjal Pipaliya <kinjalpipaliya88@gmail.com>
2024-02-15 18:12:08 +0400

    untitled sm button

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -3,17 +3,20 @@ import './index.css';
 import './font/Inter-SemiBold.ttf';
 
 function App() {
+
+   var myInput = true;
+
   return (  
     <body class="text-center">
       <div class="m-5 d-flex">
         <div class="flex-fill">
-          <a href="#0" class = "my-custom-button">Button CTA</a>
+          <a href="#0" class={(!myInput ? 'my-custom-button-disabled' : 'my-custom-button')}>Button CTA</a>
         </div>
         <div class="flex-fill">
-          <a href="#0" class = "my-custom-button">Button CTA</a>
+          {/* <a href="#0" class = "my-custom-button">Button CTA</a> */}
         </div>
         <div class="flex-fill">
-          <a href="#0" class = "my-custom-button">Button CTA</a>
+          {/* <a href="#0" class = "my-custom-button">Button CTA</a> */}
         </div>
       </div>
     </body>
