71ae4f074c0f56257ea04f1f9609eaad7244c494
Kinjal Pipaliya <kinjalpipaliya88@gmail.com>
2024-02-15 16:23:22 +0400

    Button sm

--- landing-page/src/App.js
+++ landing-page/src/App.js
@@ -1,9 +1,21 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
+import './font/Inter-SemiBold.ttf';
 
 function App() {
-  return (
+  return (  
     <body class="text-center">
-      <button type = "button" class = "btn btn-primary btn-block text-white">Button CTA</button>
+      <div class="m-5 d-flex">
+        <div class="flex-fill">
+          <a href="#0" class = "my-custom-button">Button CTA</a>
+        </div>
+        <div class="flex-fill">
+          <a href="#0" class = "my-custom-button">Button CTA</a>
+        </div>
+        <div class="flex-fill">
+          <a href="#0" class = "my-custom-button">Button CTA</a>
+        </div>
+      </div>
     </body>
   );
 }
